f024815d495fec9b6cc0f3c168a476e62cad3af2
limyeonsang <limquo@gmail.com>
2022-05-19 09:48:22 +0900

    Add: firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app'
+import {getFireStore} from "firebase/firestore"
+import {getStorage} from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI9VG2bfcBK3TsHlQtENuKRO-M_VbLEC8",
+  authDomain: "empty-car-a02ed.firebaseapp.com",
+  projectId: "empty-car-a02ed",
+  storageBucket: "empty-car-a02ed.appspot.com",
+  messagingSenderId: "937343504187",
+  appId: "1:937343504187:web:357e1ac140e9a738beb11e"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export const storage = getStorage(app);
+export const db = getFireStore(app);
